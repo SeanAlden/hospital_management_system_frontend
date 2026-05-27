33dd1022a8442eeb011b11e9c42f87219175e176
Sean Alden <c14210052@john.petra.ac.id>
2026-05-27 12:15:13 +0700

    refactor project structure for api call

--- src/config/api.js
+++ src/config/api.js
@@ -1,2 +1,2 @@
-// export const BASE_URL = "https://hospital-management-system-backend-zic1.onrender.com";
-export const BASE_URL = "http://localhost:5000";
+export const BASE_URL = "https://hospital-management-system-backend-34hc.onrender.com";
+// export const BASE_URL = "http://localhost:5000";
